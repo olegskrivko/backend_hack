bbc09fca3b4b5c55404799d34ae626378e16e27e
Olegs <olegs.krivko@gmail.com>
2025-03-26 13:40:49 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
+@app.route('/')
+def home():
+    data = {'message': 'Hello, Flask!'}
+    return jsonify(data)
+
+@app.route('/status')
+def status():
+    status_info = {'status': 'running', 'uptime': '48 hours'}
+    return jsonify(status_info)
+
+if __name__ == '__main__':
+    app.run(debug=True)
