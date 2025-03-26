1831eabe03e445f4d7b0033c794a8a1903c321e2
annabelhiiu <annabel.hiiu@ut.ee>
2025-03-26 14:57:09 +0200

    Modified customer data importing

--- app.py
+++ app.py
@@ -1,53 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from customers import user_data
 
-user_data = {
-    "user": {
-      "id": 1,
-      "name": "Jane Doe",
-      "email": "jane.doe@example.com",
-      "phone": "+1234567890",
-      "address": {
-        "street": "123 Elm Street",
-        "city": "Springfield",
-        "state": "IL",
-        "zip": "62701",
-        "country": "USA"
-      },
-      "portfolio": {
-        "stocks": [
-          {
-            "symbol": "AAPL",
-            "company": "Apple Inc.",
-            "quantity": 50,
-            "purchase_price": 145.30,
-            "current_price": 150.00,
-            "purchase_date": "2023-05-15"
-          },
-          {
-            "symbol": "GOOGL",
-            "company": "Alphabet Inc.",
-            "quantity": 10,
-            "purchase_price": 2725.00,
-            "current_price": 2800.00,
-            "purchase_date": "2022-11-20"
-          },
-          {
-            "symbol": "AMZN",
-            "company": "Amazon.com Inc.",
-            "quantity": 5,
-            "purchase_price": 3450.50,
-            "current_price": 3300.00,
-            "purchase_date": "2021-08-10"
-          }
-        ],
-        "total_investment": 0,
-        "current_value": 0,
-        "profit_loss": 0
-      }
-    }
-  }
-  
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
---
+++ customers.py
@@ -0,0 +1,47 @@
+user_data = {
+    "user": {
+      "id": 1,
+      "name": "Jane Doe",
+      "email": "jane.doe@example.com",
+      "phone": "+1234567890",
+      "address": {
+        "street": "123 Elm Street",
+        "city": "Springfield",
+        "state": "IL",
+        "zip": "62701",
+        "country": "USA"
+      },
+      "portfolio": {
+        "stocks": [
+          {
+            "symbol": "AAPL",
+            "company": "Apple Inc.",
+            "quantity": 50,
+            "purchase_price": 145.30,
+            "current_price": 150.00,
+            "purchase_date": "2023-05-15"
+          },
+          {
+            "symbol": "GOOGL",
+            "company": "Alphabet Inc.",
+            "quantity": 10,
+            "purchase_price": 2725.00,
+            "current_price": 2800.00,
+            "purchase_date": "2022-11-20"
+          },
+          {
+            "symbol": "AMZN",
+            "company": "Amazon.com Inc.",
+            "quantity": 5,
+            "purchase_price": 3450.50,
+            "current_price": 3300.00,
+            "purchase_date": "2021-08-10"
+          }
+        ],
+        "total_investment": 0,
+        "current_value": 0,
+        "profit_loss": 0
+      }
+    }
+  }
+  
